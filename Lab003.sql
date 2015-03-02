7b8364420651614a9ce0bef801e213fde93845a8
jibbey <john.kryspin1@marist.edu>
2015-03-02 17:54:47 -0500

    Started lab003

---
+++ Lab003.sql
@@ -0,0 +1,25 @@
+﻿--Question 1
+select ordno, dollars
+from orders
+
+--Question 2
+select name, city
+from agents
+where name = 'Smith'
+
+--Question 3
+select pid, name, priceUSD
+from products
+where quantity > 200000
+
+--Question 4
+select name, city
+from customers
+where city = 'Dallas'
+
+--Question 5
+select name
+from agents
+where (city != 'New York' and city != 'Tokyo')
+
+--Question 6
