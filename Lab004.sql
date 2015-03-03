cd99312dbb87c06cff533481b1cb55db93eb104e
jibbey <john.kryspin1@marist.edu>
2015-03-02 19:02:09 -0500

    lab04 needs work

---
+++ Lab004.sql
@@ -0,0 +1,41 @@
+﻿--Question 1
+select city 
+from agents
+where aid in (select aid
+              from orders
+              where cid = 'c006');
+
+--Question 2
+select pid
+from products
+where pid in (select pid 
+              from orders
+              where cid in (select cid 
+			    from customers 
+			    where city = 'Kyoto'))
+			    order by pid DESC
+
+--Question 3
+select cid, name
+from customers
+where cid not in (select cid
+              from orders
+              where aid = 'a03')
+
+--Question 4
+select cid 
+from orders
+where pid = 'p01' intersect select cid 
+                            from orders
+                            where pid = 'p07'
+
+--Question 5 needs work!
+select pid
+from orders
+where 
+
+select cid 
+from orders
+where cid not in (select cid
+                  from orders
+                  where aid = 'a05')
